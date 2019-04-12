d0e673f1ca58b10d1f6cbd4c29416829d4e8234a
WIN10 <a.ahmadi.k.79@gmail.com>
2019-04-12 19:19:13 +0430

    Account 1

--- src/model/Account.java
+++ src/model/Account.java
@@ -12,24 +12,43 @@ public class Account {
     private String userName;
     private String passWord;
 
+    Account(String userName) {
+        this.userName = userName;
+    }
 
-    public static Account findAccount(String userName) {
-        return null;
+    public static Account findAccount(String userName) {           //if not valid return null
+        Account temp = new Account("0");
+        for (Account key : accounts) {
+            if (key != null && key.userName.equals(userName))
+                temp = key;
+        }
+        if (isUserNameValid(userName))
+            return temp;
+        else
+            return null;
     }
 
     public static boolean isUserNameValid(String userName) {
+        for (Account key : accounts) {
+            if (key != null && key.userName.equals(userName))
+                return true;
+        }
         return false;
     }
 
     public static boolean isPassWordValid(String passWord) {
+        for (Account key : accounts) {
+            if (key != null && key.passWord.equals(passWord))
+                return true;
+        }
         return false;
     }
 
-    public static ArrayList<Account> showAllAccounts() {
+    public static ArrayList<Account> showAllAccounts() {         //not complete
         return null;
     }
 
-    public static void login(String userName, String passWord) {
+    public static void login(String userName, String passWord) {    //not complete
 
     }
 
@@ -49,19 +68,21 @@ public class Account {
     }
 
     public static ArrayList<Account> getAccounts() {
-        return null;
+        return accounts;
     }
 
     public void reduceMoney(int money) {
+        this.money -= money;
     }
 
     public void addMoney(int money) {
+        this.money += money;
     }
 
     public void createNewDeck(String name) {
     }
 
-    public static Deck[] getDecks() {
-        return null;
+    public Deck[] getDecks() {
+        return this.decks;
     }
 }
