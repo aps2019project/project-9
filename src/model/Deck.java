5e13b21bcb5e36eec18f5f1047deeca87e6341fc
WIN10 <a.ahmadi.k.79@gmail.com>
2019-04-12 19:57:12 +0430

    deck is completing...

--- src/model/Deck.java
+++ src/model/Deck.java
@@ -1,4 +1,48 @@
 package model;
 
+import model.cards.Card;
+import model.cards.Hero;
+import model.items.Item;
+
+import java.util.ArrayList;
+
 public class Deck {
+    ArrayList<Card> cards;
+    Hero hero;
+    Item item;
+    String name;
+
+    public void sort() {
+    }
+
+    public void addCard(Card card) {
+        for (Card key : cards) {
+            if (key == null) {
+                key = card;
+                break;
+            }
+        }
+    }
+
+    public void addItem(Item item) {
+        this.item = item;
+    }
+
+    public void removeCard(Card card) {
+        if (cards.contains(card))
+            cards.remove(card);
+    }
+
+    public void removeItem(Item item) {        //not compelete
+        item = null;
+    }
+    public boolean isValid(){
+        return false;
+    }              //I have no idea
+    public String toString(){
+        return null;
+    }
+    public Card getCard(String cardID){      //need card to complete
+        return  null;
+    }
 }
