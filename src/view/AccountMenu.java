03178ae439c78441d4b49f228572a97c95f30d90
WIN10 <a.ahmadi.k.79@gmail.com>
2019-04-12 13:20:17 +0430

    Account & AccountMenu

--- src/view/AccountMenu.java
+++ src/view/AccountMenu.java
@@ -1,4 +1,24 @@
 package view;
 
 public class AccountMenu {
+    public void createAccount() {
+    }
+
+    public void login() {
+    }
+
+    public void showLeaderBoards() {
+    }
+
+    public void getInput() {
+    }
+
+    public void showMenu() {
+    }
+
+    public void showHelp() {
+    }
+
+    public void logOut() {
+    }
 }
