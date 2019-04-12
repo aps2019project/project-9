e356ecd669eb6b135c8f0e6bebcdf0a535e460ba
WIN10 <a.ahmadi.k.79@gmail.com>
2019-04-12 14:27:41 +0430

    shop & shop menu basic

--- src/model/Shop.java
+++ src/model/Shop.java
@@ -1,4 +1,37 @@
 package model;
 
+import model.cards.Card;
+import model.items.Item;
+
+import java.util.ArrayList;
+
 public class Shop {
+    private ArrayList<Card> allCards;
+    private ArrayList<Item> allItems;
+
+    public void search(Card card) {
+    }
+
+    public void search(Item item) {
+    }
+
+    public void buy(Card card, Account account) {
+    }
+
+    public void buy(Item item, Account account) {
+    }
+
+    public void sell(Card card, Account account) {
+    }
+
+    public void sell(Item item, Account account) {
+    }
+
+    public ArrayList<Card> getCards() {
+        return allCards;
+    }
+
+    public ArrayList<Item> getItems() {
+        return allItems;
+    }
 }
--- src/view/ShopMenu.java
+++ src/view/ShopMenu.java
@@ -1,4 +1,30 @@
 package view;
 
+import model.Account;
+
 public class ShopMenu {
+    public static ShopMenu menu;
+    public Account accountLoggedIn;
+
+    public void exit() {
+    }
+
+    public void showCollection() {
+    }
+
+    public void buy() {
+    }
+
+    public void sell() {
+    }
+
+    public void showShop() {
+    }
+
+    public void help() {
+    }
+
+    public void showMenu() {
+    }
+
 }
