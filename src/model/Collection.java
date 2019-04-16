4d634230b1fbb01d37be98d4e2e57ca9fb3d24f8
aliraad79 <a.ahmadi.k.79@gmail.com>
2019-04-16 17:15:53 +0430

    collection is complete

--- src/model/Collection.java
+++ src/model/Collection.java
@@ -1,4 +1,39 @@
 package model;
 
+import model.cards.Card;
+import model.items.Item;
+
+import java.util.ArrayList;
+
 public class Collection {
+    private ArrayList<Card> cards;
+    private ArrayList<Item> items;
+    private Account ownerAccount;
+
+    public Card getCard(String cardID) {
+        for (Card key : cards) {
+            if (key != null) {     //&& key.getcardID
+                return key;
+            }
+        }
+        return null;
+    }   //returns null if not in collection
+
+    public Item getItem(String itemID) {
+        for (Item key : items) {
+            if (key != null) {     //&& key.getItemID
+                return key;
+            }
+        }
+        return null;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void removeCard(Card card) {
+        cards.remove(card);
+    }
+
 }
