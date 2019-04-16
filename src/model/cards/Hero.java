321d40e2799eff96a2e0a9b32187708ca71232cc
MohsenDehghankar <45297389+MohsenDehghankar@users.noreply.github.com>
2019-04-16 13:25:15 +0430

    test2

--- src/model/cards/Hero.java
+++ src/model/cards/Hero.java
@@ -1,4 +1,16 @@
 package model.cards;
 
+import model.Cell;
+
+import java.util.ArrayList;
+
 public class  Hero extends Minion{
+    private static ArrayList<Hero> heroes;
+    private int cooldown;
+    private Spell heroSpell;
+
+    public boolean isSpellReady(){return false;}
+    public void castSpell(Cell cell){}
+    public void useSpecialPower(Cell cell){}
+    public String toString(){return null;}
 }
---
+++ src/model/specialPower/SpecialPower.java
@@ -0,0 +1,12 @@
+package model.specialPower;
+
+import model.Cell;
+import model.cards.Minion;
+import model.enumerations.SpecialPowerActivationTime;
+
+public abstract class SpecialPower {
+
+    protected SpecialPowerActivationTime specialPowerActivationTime;
+    protected Minion minion;
+    public abstract void castSpecialPower(Cell cell);
+}
