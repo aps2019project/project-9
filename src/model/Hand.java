48deb3e5fbc82a179cda2ba9086d82fa8c4234b4
aliraad79 <a.ahmadi.k.79@gmail.com>
2019-04-16 21:25:47 +0430

    basic of Hand

--- src/model/Hand.java
+++ src/model/Hand.java
@@ -1,4 +1,26 @@
 package model;
 
+import model.cards.Card;
+
+import java.util.ArrayList;
+
 public class Hand {
+    private ArrayList<Card> cards;
+    private Deck deck;
+
+    public Card getNext() {
+        return null;
+    }
+
+    public void addCardFromDeck() {
+
+    }
+
+    public String toString() {
+        return null;
+    }
+
+    public void deleteCard(Card card) {
+        cards.remove(card);
+    }
 }
