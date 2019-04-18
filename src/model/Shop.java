0fe187703b798bffdca0be377d8c89f67f399685
aliraad79 <a.ahmadi.k.79@gmail.com>
2019-04-18 12:33:58 +0430

    completing shop phase 1

--- src/model/Shop.java
+++ src/model/Shop.java
@@ -15,16 +15,24 @@ public class Shop {
     public void search(Item item) {
     }
 
-    public void buy(Card card, Account account) {
+    public void buy(Card card, Account account) {          //need card to be complete
+        account.addMoney(card.getPrice);
+        account.getCollection().addCard(card);
     }
 
-    public void buy(Item item, Account account) {
+    public void buy(Item item, Account account) {           //need item to be complete
+        account.addMoney(item.getPrice);
+        account.getCollection().addItem(item);
     }
 
-    public void sell(Card card, Account account) {
+    public void sell(Card card, Account account) {      //need card to be complete
+        account.reduceMoney(card.getPrice);
+        account.getCollection().removeCard(card);
     }
 
-    public void sell(Item item, Account account) {
+    public void sell(Item item, Account account) {            //need item to be complete
+        account.reduceMoney(item.getPrice);
+        account.getCollection().removeItem(item);
     }
 
     public ArrayList<Card> getCards() {
