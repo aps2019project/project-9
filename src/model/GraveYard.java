c5b1018e686597f20e7726a545057c7f2050384e
hamedrashidpour <45601809+hamedrashidpour@users.noreply.github.com>
2019-04-20 02:40:21 -0700

    start grave yard

--- src/model/GraveYard.java
+++ src/model/GraveYard.java
@@ -1,4 +1,20 @@
 package model;
 
+import model.cards.Card;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+
 public class GraveYard {
+    private Player player;
+    private ArrayList<Card> cards;
+    public ArrayList<Card> getCards(){
+
+    }
+    public Card getCard(String CardID){
+
+    }
+    public void addCard(Card card){
+        
+    }
 }
