3becda7f5c5824f82b2fc3fe72973264c9663d41
MohsenDehghankar <45297389+MohsenDehghankar@users.noreply.github.com>
2019-04-20 12:52:47 +0430

    BatleMenuController completed except Creating Games ( single/multi )

--- src/view/BattleMenuRequest.java
+++ src/view/BattleMenuRequest.java
@@ -1,9 +1,112 @@
 package view;
 
+import model.enumerations.BattleMenuErrorType;
+import model.enumerations.BattleMenuRequestType;
+
+import java.util.Scanner;
+
 public class BattleMenuRequest {
-    private static final String SINGLE_PLAYER = "single player";
-    private static final String MULTI_PLAYER = "multi player";
+    Scanner scanner = new Scanner(System.in);
+    private static final String SINGLE_PLAYER = "enter single player";
+    private static final String MULTI_PLAYER = "enter multi player";
     private static final String STORY = "story";
     private static final String CUSTOM_GAME = "custom game";
-    private static final
+    private static final String START_GAME = "start game";
+    private static final String ENTER = "enter";
+    private static final String EXIT = "exit";
+    private String deckName;
+    private String mode;
+    private String numberOfFlags;
+    private String command;
+    private String userName;
+
+    public void getNewCommand() {
+        command = scanner.nextLine().toLowerCase();
+    }
+
+    public BattleMenuRequestType getTypeInSingleMultiPlayerMenu() {
+        if (command.substring(0, 5).matches(ENTER) && command.split(" ").length == 3) {
+            if (command.matches(SINGLE_PLAYER))
+                return BattleMenuRequestType.SINGLE_PLAYER;
+            else
+                return BattleMenuRequestType.MUTLI_PLAYER;
+        } else if (command.matches(EXIT)) {
+            return BattleMenuRequestType.EXIT;
+        } else {
+            return null; // invalid command
+        }
+    }
+
+    public BattleMenuRequestType getTypeSinglePlayerMenu() {
+        if (command.substring(0, 5).matches(ENTER) && command.split(" ").length == 3) {
+            if (command.matches(CUSTOM_GAME))
+                return BattleMenuRequestType.CUSTOM_GAME;
+            else
+                return BattleMenuRequestType.STORY_GAME;
+        } else if (command.matches(EXIT)) {
+            return BattleMenuRequestType.EXIT;
+        } else {
+            return null; // invalid command
+        }
+    }
+
+    public BattleMenuRequestType getTypeStoryMode() {
+        if (command.matches("1")) {
+            return BattleMenuRequestType.FIRST_LEVEL;
+        } else if (command.matches("2")) {
+            return BattleMenuRequestType.SECOND_LEVEL;
+        } else if (command.matches("3")) {
+            return BattleMenuRequestType.THIRD_LEVEL;
+        } else
+            return null;
+    }
+
+    public BattleMenuRequestType getTypeOfCustomGame() {
+        String[] commands = command.split(" ");
+        if (commands.length == 5 && commands[0].equals("start") && commands[1].equals("game")) {
+            numberOfFlags = commands[4];
+            deckName = commands[2];
+            mode = commands[3];
+            if (numberOfFlags.matches("[0-9]{2}") && mode.matches("[1-3]"))
+                return BattleMenuRequestType.START_GAME;
+            else
+                return null;
+        } else if (command.matches(EXIT)) {
+            return BattleMenuRequestType.EXIT;
+        } else
+            return null;
+    }
+
+    public String getDeckName() {
+        return deckName;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public String getNumberOfFlags() {
+        return numberOfFlags;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public BattleMenuRequestType getTypeMultiPlayer() {
+        // for select user [ username ] and Start MultiPlayer game
+        if (command.split(" ").length == 3
+                && command.substring(0, 11).matches("select user")) {
+            userName = command.split("")[2];
+            return BattleMenuRequestType.SELECT_USER;
+        } else if (command.split(" ").length == 5
+                && command.substring(0, 22).matches("start multiplayer game")) {
+            mode = command.split(" ")[3];
+            numberOfFlags = command.split(" ")[4];
+            return BattleMenuRequestType.START_GAME;
+        }else if(command.matches(EXIT))
+            return BattleMenuRequestType.EXIT;
+        else
+            return null;
+    }
 }
--- src/view/BattleMenuView.java
+++ src/view/BattleMenuView.java
@@ -1,5 +1,9 @@
 package view;
 
+import model.Account;
+import model.Deck;
+import model.enumerations.BattleMenuErrorType;
+
 public class BattleMenuView {
     private static final BattleMenuView BATTLE_MENU_VIEW = new BattleMenuView();
     private static final String FIRST_LEVEL = "1 . Level : 1, Hero : WHITE_DEEV, mode : 1";
@@ -7,27 +11,51 @@ public class BattleMenuView {
     private static final String THIRD_LEVEL = "3 . Level : 3, Hero : ARASH, mode : 3";
 
 
-    public static BattleMenuView getInstance(){
+    public static BattleMenuView getInstance() {
         return BATTLE_MENU_VIEW;
     }
 
-    public void showSingleMultiPlayerMenu(){
+    public void showSingleMultiPlayerMenu() {
         System.out.println("1 . Single Player");
         System.out.println("2 . Multi Player");
+        System.out.println("3 . Exit");
         System.out.println("To Enter An Option : Enter [ Option ]");
     }
 
-    public void showSinglePlayerMenu(){
+    public void showSinglePlayerMenu() {
         System.out.println("1 . Story\n2 . Custom Game");
         System.out.println("to enter an option : Enter [ Option ]");
     }
 
-    public void showStoryMenu(){
+    public void showStoryMenu() {
         // show the levels
         System.out.println(FIRST_LEVEL);
         System.out.println(SECOND_LEVEL);
         System.out.println(THIRD_LEVEL);
         System.out.println("Enter Option Number ...");
+    }
+
+    public void showCustomGameMenu(Account loggedInAccount) {
+        System.out.println("Your Main Deck :");
+        System.out.println(loggedInAccount.getMainDeck().toString());
+        System.out.println("Your Other Decks :");
+        for (Deck deck : loggedInAccount.getDecks()) {
+            System.out.println(deck.toString());
+        }
+        System.out.println("To Start Enter This Format:\nStart game [deck name] [mode] [number of flags]");
+    }
+
+    public void printError(BattleMenuErrorType errorType) {
+        System.out.println(errorType.getMessage());
+    }
+
+    public void showUserNames() {
+        int counter = 1;
+        System.out.println("Available Accounts :");
+        for (Account account : Account.getAccounts()) {
+            System.out.println(counter++ + " . " + account.getUserName());
+        }
+        System.out.println("Enter : \nSelect user [ username ]");
     }
 
 }
