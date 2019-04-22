196c7483ad6beb588e2b2c68dffc72d8e0bb6a4c
MohsenDehghankar <45297389+MohsenDehghankar@users.noreply.github.com>
2019-04-22 16:23:56 +0430

    multiplayer constructor

--- src/model/MultiPlayerBattle.java
+++ src/model/MultiPlayerBattle.java
@@ -1,8 +1,25 @@
 package model;
 
+import model.enumerations.GameMode;
+
 public class MultiPlayerBattle extends Battle {
 
-
+    public MultiPlayerBattle(Account firstPlayer , String secondPlayerUserName, int mode , int flags){
+        Account secondAccount = Account.findAccount(secondPlayerUserName);
+        if(mode == 1)
+            gameMode = GameMode.HERO_KILL;
+        else if(mode == 2)
+            gameMode = GameMode.ONE_FLAG;
+        else
+            gameMode = GameMode.FLAGS;
+        turn  = 0;
+        playGround = new PlayGround(gameMode);
+        whoseTurn = 1;
+        if(mode == 3)
+            numberOfFlags = flags;
+        this.firstPlayer = new Player(firstPlayer);
+        this.secondPlayer = new Player(secondAccount);
+    }
     public void doTheSecondPlayerAction(){
         // the ai for the second player
 
