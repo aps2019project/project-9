2b0d6979d87051a18c75e8626d372930f3d3049a
aliraad79 <a.ahmadi.k.79@gmail.com>
2019-04-29 09:52:42 +0430

    nothing

--- src/model/cellaffects/HollyCellAffect.java
+++ src/model/cellaffects/HollyCellAffect.java
@@ -15,7 +15,7 @@ public class HollyCellAffect extends CellAffect {
     public void castCellAffect(Minion minion) {
         // while a minion enters this cell ( cell that has this CellAffect )
         minion.setReductionOfOthersAttack(1);
-        HollyBuff hollyBuff = new HollyBuff(1,false,false);
+        HollyBuff hollyBuff = new HollyBuff(1,false,false,false);  //be checked
         hollyBuff.startBuff(minion.getCell());
     }
 
