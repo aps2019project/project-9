0cc469dabc562f2c3186e9d52fc067f651080864
MohsenDehghankar <45297389+MohsenDehghankar@users.noreply.github.com>
2019-04-29 10:05:25 +0430

    on attack special power

--- src/model/specialPower/OnAttackSpecialPower.java
+++ src/model/specialPower/OnAttackSpecialPower.java
@@ -1,32 +1,39 @@
 package model.specialPower;
 
 import model.Cell;
+import model.buffs.Buff;
+import model.cards.Minion;
 import model.cards.Spell;
 import model.enumerations.MinionName;
 import model.enumerations.SpecialPowerActivationTime;
+import model.items.ManaItems;
+
+import java.util.ArrayList;
 
 public class OnAttackSpecialPower extends SpecialPower {
-    private Spell onAttackSpell;
+    private ArrayList<Buff> buffs;
     private boolean isAntiHolly; // only for DARANDE_SHIR it should be true
     private boolean isDispel; // only for GHOOL_DOSAR
 
-    public OnAttackSpecialPower(Spell onAttackSpell, boolean isAntiHolly, boolean isDispel) {
+    public OnAttackSpecialPower(ArrayList<Buff> buffs, boolean isAntiHolly, boolean isDispel) {
         // for DARANDE_SHIR and GHOOL_DOSAR there should be no spell ( null )
         super(SpecialPowerActivationTime.ON_ATTACK);
         this.isAntiHolly = isAntiHolly;
         this.isDispel = isDispel;
-        this.onAttackSpell = onAttackSpell;
+        this.buffs = buffs;
     }
 
 
     @Override
     public void castSpecialPower(Cell cell) {
         // cell is the target cell that the minion wants to attack() to
-
         if (minion.getMinionName() == MinionName.DOSAR_GHOOL)
             cell.getMinionOnIt().dispelPositiveBuffs();
-        else if (onAttackSpell != null)
-            onAttackSpell.castSpell(cell);
+        else if (buffs != null && cell.hasCardOnIt()){
+            for (Buff buff : buffs) {
+                buff.startBuff(cell);
+            }
+        }
     }
 
     public boolean isAntiHolly() {
