e57c785a9b683eb9fb7e96d17aad711a9c042197
aliraad79 <a.ahmadi.k.79@gmail.com>
2019-05-30 15:23:54 +0430

    account menu phase 2

--- src/view/AccountMenu.java
+++ src/view/AccountMenu.java
@@ -1,16 +1,21 @@
 package view;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -39,7 +44,7 @@ public class AccountMenu extends Application {
             Group group = new Group(imageView);
             Scene scene = new Scene(group, height, width);
             scene.setFill(Color.DEEPPINK);
-            scene.setOnMouseClicked(m -> accountMenuShow(stage));
+            scene.setOnMouseClicked(event -> accountMenuShow(stage));
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
@@ -49,21 +54,32 @@ public class AccountMenu extends Application {
 
     void accountMenuShow(Stage stage) {
         try {
-            Text text = new Text(100, 50, "Account Menu");
-            text.setFont(Font.loadFont(new FileInputStream(new File("src/res/modern.TTF")), 50));
-            text.setX(651);
-            text.setY(100);
+            Font accountMenuFont = Font.loadFont(new FileInputStream(new File("src/res/modern.TTF")), 40);
+            Text text = new Text(651, 100, "Account Menu");
+            text.setFont(accountMenuFont);
             text.setFill(Color.rgb(2, 14, 236));
             ImageView imageView = new ImageView();
             imageView.setImage(new Image(new FileInputStream("src/res/5.jpg")));
             imageView.setFitWidth(1700);
             imageView.setFitHeight(1050);
-            Button createAccount = new Button();
-            createAccount.setText("Create Account");
-            createAccount.setLayoutX(100);
-            createAccount.setLayoutY(100);
 
-            stage.setScene(new Scene(new Group(imageView, text, createAccount), height, width));
+            ////most converted to button
+            Text createAccount = new Text(630, 200, "Create account");
+            createAccount.setFont(accountMenuFont);
+
+            Text login = new Text(630, 300, "Login");
+            login.setFont(accountMenuFont);
+
+            Text showLeaderBoard = new Text(630, 400, "show LeaderBoard");
+            showLeaderBoard.setFont(accountMenuFont);
+
+            Text help = new Text(630, 500, "Help");
+            help.setFont(accountMenuFont);
+
+            //to here
+
+            Group root = new Group(imageView, text, createAccount, login, showLeaderBoard, help);
+            stage.setScene(new Scene(root, height, width));
             stage.show();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
